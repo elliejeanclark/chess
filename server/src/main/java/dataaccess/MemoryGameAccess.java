35bf7acc3eeb48cf2ce0b1a5d7602b8493bd7d97
Ellie <elinorj@byu.edu>
2025-02-24 18:02:33 -0700

    starting to implement MemoryGameAccess

---
+++ server/src/main/java/dataaccess/MemoryGameAccess.java
@@ -0,0 +1,35 @@
+package dataaccess;
+
+import chess.ChessGame;
+import model.*;
+import java.util.HashMap;
+
+public class MemoryGameAccess implements GameDataAccess {
+    private final HashMap<Integer, GameData> games;
+
+    public MemoryGameAccess() {
+        games = new HashMap<>();
+    }
+
+    public void createGame(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        GameData gameData = new GameData(gameID, whiteUsername, blackUsername, gameName, game);
+        games.put(gameID, gameData);
+    }
+
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    public HashMap<Integer, GameData> getGames(){
+        return games;
+    }
+
+    public void updateGame(int gameID, ChessGame game) {
+        GameData oldData = games.get(gameID);
+        String whiteUsername = oldData.whiteUsername();
+        String blackUsername = oldData.blackUsername();
+        String gameName = oldData.gameName();
+        GameData newData = new GameData(gameID, whiteUsername, blackUsername, gameName, game);
+        games.put(gameID, newData);
+    }
+}
