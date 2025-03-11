15c3d4990b7c7573472b3c73af8a99826a3a95d9
Ellie <elinorj@byu.edu>
2025-03-11 14:00:53 -0600

    ready to start figuring out serialization

--- server/src/main/java/dataaccess/SQLGameAccess.java
+++ server/src/main/java/dataaccess/SQLGameAccess.java
@@ -1,11 +1,53 @@
 package dataaccess;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
 public class SQLGameAccess implements GameDataAccess {
+
+    public SQLGameAccess() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var us = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) us.setString(i + 1, p);
+                    else if (param instanceof Integer p) us.setInt(i + 1, p);
+                    //handle if it is a chess game...
+                    else if (param == null) us.setNull(i + 1, NULL);
+                }
+                us.executeUpdate();
+
+                var rs = us.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private String serializeChessGame(ChessGame game) {
+        return new Gson().toJson(game);
+    }
+
+    private ChessGame deserializeChessGame(String serializedGame) {
+        return new Gson().fromJson(serializedGame, ChessGame.class);
+    }
+
     public void createGame(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
 
     }
@@ -32,5 +74,31 @@ public class SQLGameAccess implements GameDataAccess {
 
     public void clear() {
 
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS games (
+              `gameID` int NOT NULL AUTO_INCREMENT,
+              `whiteUsername` varchar(256),
+              `blackUsername` varchar(256),
+              `gameName` varchar(256),
+              `game` TEXT DEFAULT NULL,
+              PRIMARY KEY (`gameID`)
+            )
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
