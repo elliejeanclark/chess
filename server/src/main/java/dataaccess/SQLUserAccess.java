b781fb38e6b3a76e16e36132b560f43b29b32d88
Ellie <elinorj@byu.edu>
2025-03-11 13:03:45 -0600

    user access almost working

--- server/src/main/java/dataaccess/SQLUserAccess.java
+++ server/src/main/java/dataaccess/SQLUserAccess.java
@@ -1,4 +1,166 @@
 package dataaccess;
 
-public class SQLUserAccess {
+import model.UserData;
+
+import java.sql.*;
+import java.util.HashMap;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class SQLUserAccess implements UserDataAccess {
+
+    SQLUserAccess() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var us = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) us.setString(i + 1, p);
+                    else if (param == null) us.setNull(i + 1, NULL);
+                }
+                us.executeUpdate();
+
+                var rs = us.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    public void createUser(UserData user) {
+        String username = user.username();
+        String password = user.password();
+        String email = user.email();
+
+        var statement = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        try {
+            executeUpdate(statement, username, email, password);
+        } catch (DataAccessException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void removeUser(String username) throws DataAccessException {
+        var statement = "DELETE FROM users WHERE username=?";
+        executeUpdate(statement, username);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, email, password FROM users WHERE username=?";
+            try (var us = conn.prepareStatement(statement)) {
+                us.setString(1, username);
+                try (var rs = us.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    private UserData readUser(ResultSet rs) throws SQLException {
+        var username = rs.getString("username");
+        var email = rs.getString("email");
+        var password = rs.getString("password");
+        return new UserData(username, password, email);
+    }
+
+    public boolean checkUsernameTaken(String username) {
+        try {
+            UserData user = getUser(username);
+            if (user != null) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        catch (DataAccessException e) {
+            return true;
+        }
+    }
+
+    public HashMap<String, UserData> getUsers() {
+        var result = new HashMap<String, UserData>();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM users";
+            try (var us = conn.prepareStatement(statement)) {
+                try (var rs = us.executeQuery()) {
+                    while (rs.next()) {
+                        result.put(readUser(rs).username(), readUser(rs));
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            return result;
+        }
+        return result;
+    }
+
+    public void clear() {
+        var statement = "Truncate users";
+        try {
+            executeUpdate(statement);
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+              `username` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`)
+            )
+            """
+//            """
+//            CREATE TABLE IF NOT EXISTS auth (
+//              `authID` varchar(256) NOT NULL,
+//              `username` varchar(256) NOT NULL,
+//              PRIMARY KEY (`authID`),
+//              FOREIGN KEY username REFERENCES users(username)
+//            )
+//            """,
+//            """
+//            CREATE TABLE IF NOT EXISTS games (
+//              `gameID` int NOT NULL AUTO_INCREMENT,
+//              `whiteUsername` varchar(256),
+//              `blackUsername` varchar(256),
+//              `gameName` varchar(256),
+//              `game` TEXT DEFAULT NULL,
+//              PRIMARY KEY (`gameID`)
+//            )
+//            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
 }
---
+++ server/src/test/java/dataaccess/SQLUserAccessTest.java
@@ -0,0 +1,55 @@
+package dataaccess;
+
+import dataaccess.SQLUserAccess;
+
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQLUserAccessTest {
+
+    private SQLUserAccess userAccess;
+
+    @BeforeEach
+    void setup() {
+        try {
+            userAccess = new SQLUserAccess();
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        userAccess.clear();
+    }
+
+    @Test
+    void createUser() throws DataAccessException {
+        UserData testUser = new UserData("user", "password", "bob");
+        userAccess.createUser(testUser);
+        UserData returnedUser = userAccess.getUser(testUser.username());
+        Assertions.assertEquals(testUser, returnedUser);
+    }
+
+    @Test
+    void removeUser() throws DataAccessException {
+        UserData testUser = new UserData("user", "password", "bob");
+        userAccess.createUser(testUser);
+        userAccess.removeUser(testUser.username());
+        int expectedSize = 0;
+        Assertions.assertEquals(expectedSize, userAccess.getUsers().size());
+    }
+
+    @Test
+    void checkUsernameTaken() {
+        UserData testUser = new UserData("user", "password", "bob");
+        userAccess.createUser(testUser);
+        Assertions.assertThrows(RuntimeException.class, () -> {userAccess.createUser(testUser);});
+    }
+}
