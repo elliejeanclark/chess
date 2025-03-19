660c76858bb2c7af24d64c4cdde1645faa6d1305
Ellie <elinorj@byu.edu>
2025-03-18 18:40:02 -0600

    Starting to implement client.

---
+++ client/src/main/java/ui/ChessClient.java
@@ -0,0 +1,27 @@
+package ui;
+
+import client.ServerFacade;
+
+import java.util.Arrays;
+
+public class ChessClient {
+    private String username = null;
+    private final ServerFacade server;
+    private final String serverUrl;
+    private State state = State.SIGNEDOUT;
+
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
+
+    public String eval(String input) {
+        var tokens = input.toLowerCase().split(" ");
+        var cmd = (tokens.length > 0) ? tokens[0] : "help";
+        var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+        return switch (cmd) {
+            case "hello" -> "world";
+            default -> "world";
+        };
+    }
+}
