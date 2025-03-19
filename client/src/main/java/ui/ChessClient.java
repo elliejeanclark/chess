6216518122416a3c3b93416e75b7604e6f148075
Ellie <elinorj@byu.edu>
2025-03-18 19:14:43 -0600

    starting to add repl

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -16,12 +16,44 @@ public class ChessClient {
     }
 
     public String eval(String input) {
-        var tokens = input.toLowerCase().split(" ");
-        var cmd = (tokens.length > 0) ? tokens[0] : "help";
-        var params = Arrays.copyOfRange(tokens, 1, tokens.length);
-        return switch (cmd) {
-            case "hello" -> "world";
-            default -> "world";
-        };
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "login" -> signIn(params);
+                default -> help();
+            };
+        }
+        catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String help() {
+        if (state == State.SIGNEDOUT) {
+            return """
+                    login <USERNAME> <PASSWORD> - to login and play chess
+                    register <USERNAME> <PASSWORD> <EMAIL> - to create a new account
+                    quit - exit the probram
+                    help - list possible commands
+                    """;
+        }
+        return "here are some commands for once you have logged in";
+    }
+
+    public String signIn(String... params) throws ResponseException {
+        if (params.length >= 1) {
+            state = State.SIGNEDIN;
+            username = String.join("-", params);
+            return String.format("You signed in as %s.", username);
+        }
+        throw new ResponseException(400, "Expected: <username>");
+    }
+
+    private void assertSignedIn() throws ResponseException {
+        if (state == State.SIGNEDOUT) {
+            throw new ResponseException(400, "You must sign in");
+        }
     }
 }
